2042bc30f42f82c28abd9452716f0c4d2534ec2e
David Goehrig <dave@dloh.org>
2013-10-14 01:27:50 -0400

    fixing producer

--- lib/producer.ex
+++ lib/producer.ex
@@ -1,5 +1,8 @@
-defmodule Producer do
-	def start(uri,fun) do
-			
+defrecord Producer, amqp: nil do
+	def start(uri,self) do
+		self.amqp(Amqp.Server.new.connect(uri))
+	end
+	def send(message,self) do
+		self.amqp.send self.amqp.uri.exchange, self.amqp.uri.key, message
 	end
 end
