45baab0fdc575f17300e778c0e6d8f5d16475cfd
David Goehrig <dave@dloh.org>
2013-10-16 19:11:50 -0400

    adding functional pipe implementation

--- lib/pipe.ex
+++ lib/pipe.ex
@@ -1,3 +1,9 @@
-defmodule Pipe do
-
+defrecord Pipe, amqp: nil do
+	def start(uri,fun,self) do
+		self = self.amqp(Amqp.Server.new.connect(uri))
+		self.amqp.receive(self.amqp.uri.exchange, self.amqp.uri.key, self.amqp.uri.queue)
+		self.amqp.wait fn(message,properties) ->
+			self.amqp.send(self.amqp.uri.destination,self.amqp.uri.route, fun.(message,properties))
+		end	
+	end
 end
